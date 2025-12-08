88884081d4ead2f51f8cb7f9b84383b0493f8185
dionis.krasava <dionis.krasava@gmail.com>
2025-12-08 07:57:47 +0700

    1-4 уроки с Metanit

---
+++ src/main/kotlin/metanit/chapter2_basics/03_ProgramStructure.kt
@@ -0,0 +1,13 @@
+package edu.cesspool.metanit.chapter2_basics
+
+/*
+*  до версии 1.3 в Kotlin функция main должна была принимать параметры
+* */
+
+fun main(args: Array<String>){
+    println("Как выглядели древние функции main!")
+}
+
+// инструкции (statement) - основной строительный блок любых программ
+// вызов функции - инструкция
+
