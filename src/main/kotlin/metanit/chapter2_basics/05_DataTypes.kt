b741153c3c453ff28d0857d4bf36fd47c085061c
Ivan Belov <dionis.krasava@gmail.com>
2025-12-09 12:24:03 +0700

    5 урок дописал

--- src/main/kotlin/metanit/chapter2_basics/05_DataTypes.kt
+++ src/main/kotlin/metanit/chapter2_basics/05_DataTypes.kt
@@ -1,37 +1,35 @@
 package edu.cesspool.metanit.chapter2_basics
 
-import kotlin.system.exitProcess
-
-/*
-*
-
-    Byte: хранит целое число от -128 до 127 и занимает 1 байт
-
-    Short: хранит целое число от -32 768 до 32 767 и занимает 2 байта
-
-    Int: хранит целое число от -2 147 483 648 (-2^31) до 2 147 483 647 (2^31 - 1) и занимает 4 байта
-
-    Long: хранит целое число от –9 223 372 036 854 775 808 (-2^63) до 9 223 372 036 854 775 807 (2^63-1) и занимает 8 байт
-
-* */
-
-
-/*
-*
-
-    UByte: хранит целое число от 0 до 255 и занимает 1 байт
-
-    UShort: хранит целое число от 0 до 65 535 и занимает 2 байта
-
-    UInt: хранит целое число от 0 до 2^32 - 1 и занимает 4 байта
-
-    ULong: хранит целое число от 0 до 2^64-1 и занимает 8 байт
-
-*
-* */
-
-
 fun main(){
+    numbers()
+    float()
+    double()
+    boolean()
+    char()
+    string()
+    typeInference()
+    typeConversion()
+    typeAny()
+}
+
+
+fun numbers(){
+
+    /*
+    Byte: хранит целое число от -128 до 127 и занимает 1 байт
+    Short: хранит целое число от -32 768 до 32 767 и занимает 2 байта
+    Int: хранит целое число от -2 147 483 648 (-2^31) до 2 147 483 647 (2^31 - 1) и занимает 4 байта
+    Long: хранит целое число от –9 223 372 036 854 775 808 (-2^63) до 9 223 372 036 854 775 807 (2^63-1) и занимает 8 байт
+    */
+
+
+    /*
+        UByte: хранит целое число от 0 до 255 и занимает 1 байт
+        UShort: хранит целое число от 0 до 65 535 и занимает 2 байта
+        UInt: хранит целое число от 0 до 2^32 - 1 и занимает 4 байта
+        ULong: хранит целое число от 0 до 2^64-1 и занимает 8 байт
+    * */
+
     val a: Byte = -128
     val b: Short = 32767
     val c: Int = 1_000_000
@@ -81,16 +79,8 @@ fun main(){
     println("--------------------")
     println("0101 = $num2")
     println("1011 = $num3")
-
-
-
-    //---------------------------------
-    float()
-    double()
-    boolean()
-    char()
-
 }
+
 
 
 
@@ -125,6 +115,8 @@ fun float(){
 
 
 
+
+
 /*- - - - - - - -    D O U B L E     - - - - - - - -  */
 fun double(){
     // DOUBLE
@@ -141,6 +133,9 @@ fun double(){
 }
 
 
+
+
+
 /*- - - - - - - -    Л О Г И Ч Е С К И Й     Т И П     - - - - - - - -  */
 fun boolean(){
     val a: Boolean = true
@@ -151,6 +146,9 @@ fun boolean(){
     println("b = $b")
     println()
 }
+
+
+
 
 
 /*- - - - - - - -    С И М В О Л Ы     - - - - - - - -  */
@@ -166,3 +164,93 @@ fun char(){
 
 }
 
+
+
+
+/*- - - - - - - -    С Т Р О К И     - - - - - - - -  */
+fun string(){
+
+    // можно использовать специальные символы
+    val text: String = "SALT II was a series of talks between United States \n and Soviet negotiators from 1972 to 1979"
+
+    val text2: String = """
+                        SALT II was a series of talks between United States
+                        and Soviet negotiators from 1972 to 1979.
+                        It was a continuation of the SALT I talks.
+                    """
+
+}
+
+
+
+
+/*- - - - - - - -    В Ы В Е Д Е Н И Е     Т И П А     - - - - - - - -  */
+
+
+/** Kotlin автоматически может понять, что за тип у пеменной исходя из значения
+ * которым мы инициализируем данную переменную
+ *
+ * */
+fun typeInference(){
+
+
+
+    val age = 5             // Int
+    val name = "Tom"        // String
+
+    val sum = 45L           // Long
+
+    val height = 1.81       // Double
+    val height2 = 1.80f     // Float
+
+
+    // НЕЛЬЗЯ ОБЪЯВИТЬ ПЕРЕМЕННУЮ БЕЗ ТИПА А ПОЗЖЕ ИНИЦИАЛИЗИРОВАТЬ
+    /*
+     * val age // ERROR
+     * age = 5
+     */
+
+}
+
+
+
+/*- - - - - - - -    П Р И В Е Д Е Н И Е     Т И П А      - - - - - - - -  */
+fun typeConversion(){
+    /*
+    * Присвоить значение одного типа переменной другого типа данных напрямую нельзя
+    *
+    * поэтому их явно нужно преобразовывать
+    * */
+
+    /**
+     * toByte
+     * toShort
+     * toInt
+     * toLong
+     * toFloat
+     * toDouble
+     * toChar
+     *
+     * */
+
+    var longN: Long = 2
+    val intN: Int = 4
+
+    longN = intN.toLong()
+
+
+}
+
+
+fun typeAny(){
+    /*
+    * Есть такой супертип, которому можно присвоить любое значение
+    * */
+
+    var name: Any = "Tom"
+    println(name) // Tom
+
+    name = 6758
+    println(name) // 6758
+
+}
