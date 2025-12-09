cc95fede2a5de6e9e0070a68e26179f192180c48
Ivan Belov <dionis.krasava@gmail.com>
2025-12-09 14:48:37 +0700

    8 урок 2 главы

--- src/main/kotlin/metanit/chapter2_basics/07_OperationsNumbers.kt
+++ src/main/kotlin/metanit/chapter2_basics/07_OperationsNumbers.kt
@@ -3,8 +3,93 @@ package edu.cesspool.metanit.chapter2_basics
 /**
  * ОПЕРАЦИИ
  *
- * + СЛОЖЕНИЕ
+ *      + СЛОЖЕНИЕ
  *
- * - ВЫЧИТАНИЕ
+ *      - ВЫЧИТАНИЕ
+ *
+ *      * УМНОЖЕНИЕ
+ *
+ *      / ДЕЛЕНИЕ
  *
  * */
+
+
+
+fun main(){
+
+    exampleDivision()
+    exampleDivisionIntDouble()
+    exampleRemains()
+    incremenAndDecrement()
+}
+
+
+// ОТБРАСЫВАЕТСЯ ОТСТАТОК
+fun exampleDivision(){
+    /*так как при делении целочисл на целочисл
+    * в результате получается целочисл*/
+    val x = 11
+    val y = 5
+    val z = x / y       // 2
+
+    println("=======DIVISION INT INT========")
+    println("int $x / int $y = $z")
+}
+
+
+// ЕСЛИ ОДИН ИЗ ОПЕРАНДОВ С ТОЧКОЙ, ТО И РЕЗУЛЬТАТ БУДЕТ С ТОЧКОЙ
+fun exampleDivisionIntDouble(){
+    val x = 11
+    val y = 5.0
+    val z = x / y
+
+    println("=======DIVISION INT DOUBLE========")
+    println("int $x / int $y = $z")
+}
+
+
+//ОСТАТОК ЦЕЛОЧИСЛЕННОГО ДЕЛЕНИЯ   -   %
+fun exampleRemains(){
+    val x = 11
+    val y = 4
+    val z = x % y
+
+    println("======= % ========")
+    println("int $x % int $y = $z")
+}
+
+
+fun incremenAndDecrement(){
+
+    // ++x    Сначал УВЕЛИЧИТ значение переменной, затем уже вернет увеличенное значение
+    // x++    Сначала ВЕРНЕТ значение, а затем уже увеличит
+
+    /*
+
+     var x = 5
+     val y = --x
+     println(x)      // x = 4
+     println(y)      // y = 4
+
+    */
+
+    /*
+
+    var x = 5
+    val y = x--
+    println(x)      // x = 4
+    println(y)      // y = 5
+
+*/
+
+    // a += b    эквивалентно   a = a + b
+    // a -= b    эквивалентно   a = a - b
+    // a *= b    эквивалентно   a = a * b
+    // a /= b    эквивалентно   a = a / b
+    // a %= b    эквивалентно   a = a % b
+}
+
+// ДАЛЕЕ МНОЮ БЫЛО ПРИНЯТО РЕШЕНИЕ НЕ КОНСПЕКТИРОВАТЬ
+// Поразрядные операции
+// В СИЛУ РЕДКОГО РАСПРОСТРАНЕНИЯ ИХ ИСПОЛЬЗОВАНИЯ
+// И СПЕЦИФИЧНОГО РАСПРОСТРАНЕНИЯ ИСПОЛЬЗОВАНИЯ
---
+++ src/main/kotlin/metanit/chapter2_basics/08_ConditionalExpressions.kt
@@ -0,0 +1,2 @@
+package edu.cesspool.metanit.chapter2_basics
+
