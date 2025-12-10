062aa664eb19730830433c59e8c494fc2d473197
Ivan Belov <dionis.krasava@gmail.com>
2025-12-10 11:06:08 +0700

    8 урок 2 главы закончил

--- src/main/kotlin/metanit/chapter2_basics/08_ConditionalExpressions.kt
+++ src/main/kotlin/metanit/chapter2_basics/08_ConditionalExpressions.kt
@@ -1,2 +1,37 @@
 package edu.cesspool.metanit.chapter2_basics
 
+// >
+// <
+// >=
+// <=
+// ==
+// !=
+
+
+
+// and
+// or
+// xor
+//     true - если ТОЛЬКО один из операндов равен true
+// !        - инверсия
+// not()    - эквивалентно оператору - !
+// in
+
+
+fun main(){
+    exampleIn()
+}
+
+fun exampleIn(){
+    val a = 5
+    val aIsNumber = a in 0 .. 9 // true
+
+    val aIs2znach = a in 10..99 // false
+
+    val aIsNotNumber = a !in 0..9 // false
+
+    println("============IN============")
+    println("A is number - $aIsNumber")
+    println("A is 2 znachnoe - $aIs2znach")
+    println("A is Not Number - $aIsNotNumber")
+}
