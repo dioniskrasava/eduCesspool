b55429b810e9327e73b73b9a3e48f55bfda94101
Ivan Belov <dionis.krasava@gmail.com>
2025-12-10 13:40:05 +0700

    9 урок завершил

--- src/main/kotlin/metanit/chapter2_basics/09_IfElse.kt
+++ src/main/kotlin/metanit/chapter2_basics/09_IfElse.kt
@@ -1,6 +1,6 @@
 package edu.cesspool.metanit.chapter2_basics
 
-
+//----------------------------------------------
 /*ПРОСТОЙ ПРИМЕР*/
 fun ifexample1(){
     val a = 10
@@ -12,24 +12,63 @@ fun ifexample1(){
         println("a не равно 10")
     }
 }
-
+//----------------------------------------------
 
 /*С ОПУСКАНИЕМ СКОБОК*/
 /* если блок кода содержит только одно выражение, то скобки можно опустить*/
 fun ifExample2(){
     val a = 10
 
-    if (a == 10) println("a равно 10")
-    else println("a не равно 10")
+    if (a == 10)
+        println("a равно 10")
+    else
+        println("a не равно 10")
 }
 
+
+/*ELSE IF*/
+fun ifExample3(){
+    val a = 10
+
+    if (a == 10) println("a = 10")
+    else if (a == 9) println("a = 9")
+    else if (a == 8) println("a = 8")
+    else if (a == 7) println("a = 7")
+    else println("a = XZ!")
+
+}
+
+/*ВОЗВРАЩЕНИЕ ЗНАЧЕНИЯ ИЗ IF*/
+fun ifReturnExample(){
+    val a = 10
+    val b = 25
+
+
+    // ВАЖНЫЙ МОМЕНТ!!
+    // if возвращает значение!
+    val max = if (a > b) a else b
+
+
+
+    println("the maximum number of $a and $b is $max")
+}
+
+
+//----------------------------------------------
+//----------------------------------------------
+//----------------------------------------------
+//----------------------------------------------
 fun wrap(name: String, function: () -> Unit){
     println("======$name========")
     function()
     println("===================")
 }
-
+//----------------------------------------------
+//----------------------------------------------
+//----------------------------------------------
+//----------------------------------------------
 fun main(){
     wrap("SIMPLE EXAMPLE") {ifexample1()}
     wrap("LOWERING STAPLES") {ifExample2()}
+    wrap("RETURN") {ifReturnExample()}
 }
