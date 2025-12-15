10edbb770ca5aea20a94df92599f8f6eed788641
Ivan Belov <dionis.krasava@gmail.com>
2025-12-15 17:12:44 +0700

    16

--- src/main/kotlin/metanit/chapter3_functionalProgramming/15_Vararg.kt
+++ src/main/kotlin/metanit/chapter3_functionalProgramming/15_Vararg.kt
@@ -5,6 +5,33 @@ import edu.cesspool.metanit.chapter2_basics.wrap
 fun main(){
     wrap("MANY STRING"){printString("lol","kek","cheburek!")}
     wrap("SUMM"){summator(3,5,10,-8)}
+
+    val numbers = intArrayOf(1,2,3,4,5,6,7,8,9)
+    //val numbers = arrayOf(1,2,3,4,5,6,7,8,9) // Err
+
+    /**
+     * Удивительно. Но :
+     * val numbers = intArrayOf(1,2,3,4,5,6,7,8,9)
+     * val numbers = arrayOf(1,2,3,4,5,6,7,8,9)
+     *
+     * вернут разные объекты. А именно:
+     *
+     *  IntArray
+     *  Array<Int>
+     * */
+
+    //summator(numbers) // Err
+
+    // НЕОБХОДИМО ИСПОЛЬЗОВАТЬ ЗВЕЗДОЧКУ !!!
+    //
+    //         *оператор распаковки*
+    //
+    // потому что функция ждет
+    // неопределенное количество значений, а не массив
+    // поэтому мы можем утверждать, что звездочка преобразует массив в неопределенное
+    // количество значений
+    summator(*numbers)
+
 }
 
 //many string!
@@ -25,3 +52,10 @@ fun summator(vararg numbers: Int){
 
     println("Summ = $summ")
 }
+
+// ОБЫЧНО vararg-параметр является последним в сигнатуре функции
+// НО!
+// если он не последний, то при вызове функции, нужно будет использовать
+// именнованные аргументы
+
+
