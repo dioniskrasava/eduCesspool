6097cc0ab4e1354ca54e5f2f3d38aa15643d21e4
Ivan Belov <dionis.krasava@gmail.com>
2025-12-16 09:38:07 +0700

    17+

--- src/main/kotlin/metanit/chapter3_functionalProgramming/17_SingleLine_Local_function.kt
+++ src/main/kotlin/metanit/chapter3_functionalProgramming/17_SingleLine_Local_function.kt
@@ -1,2 +1,14 @@
 package edu.cesspool.metanit.chapter3_functionalProgramming
+// ОДНОСТРОЧНЫЕ И ЛОКАЛЬНЫЕ ФУНКЦИИ
 
+fun main(){
+    println("Square 15 = ${square(15)}")
+
+}
+
+// fun имя_функции (параметры_функции) = тело_функции
+//
+// слова return                 - нет
+// типа возвращаемого значения  - нет
+
+fun square(x: Int) = x * x
