4b8b7fd29688f271a64e34eec34ded484e09ecb8
Ivan Belov <dionis.krasava@gmail.com>
2025-12-18 14:06:26 +0700

    22

---
+++ src/main/kotlin/metanit/chapter3_functionalProgramming/21_Anonymous_Functions.kt
@@ -0,0 +1,82 @@
+package edu.cesspool.metanit.chapter3_functionalProgramming
+
+fun main(){
+    example1()
+    example2()
+    example3()
+
+    exampleAgrument()
+}
+
+
+
+fun example1(){
+    // ОБЪЯВЛЕНИЕ ФУНКЦИИ
+    val funfun = fun(){
+        println("FUN-FUN-FUN-FUN!")
+    }
+    // ВЫЗОВ
+    funfun()
+}
+
+
+
+fun example2(){
+    val funfun = fun() = println("FFFFFFFUN!")
+    funfun()
+}
+
+
+fun example3(){
+    val sum = fun(a: Int, b: Int) : Int = a + b
+    val result = sum(5,9)
+    println("Summ 5 + 9 = $result")
+}
+
+
+//--------------------------------------------------
+// АНОНИМНАЯ ФУНКЦИЯ КАК АРГУМЕНТ ФУНКЦИИ
+
+fun exampleAgrument(){
+    println("EXAMPLE TO ARGUMENT!")
+    doOperating(5,7,   fun(x: Int, y: Int)       = x +  y)
+    doOperating(9, 46, fun(z: Int, zz: Int): Int = z * zz)
+
+    // С использованием функции которая возвратить анонимную функцию
+    doOperating(44,55, getAnFun(1))
+    doOperating(5,45, getAnFun(547))
+
+}
+
+fun doOperating(a: Int, b: Int, operation: (Int, Int) -> Int){
+    println("Result = ${operation(a,b)}")
+}
+
+//-------------------------------------------------
+// АНОНИМНУЮ ФУНКЦИЮ МОЖНО ВЕРНУТЬ
+
+fun getAnFun(a: Int) : (Int, Int) -> Int{
+    when (a){
+        1 -> return fun(a: Int, b: Int) = a + b
+        2 -> return fun(a: Int, b: Int) = a - b
+        else -> return fun(a: Int, b: Int) = 0
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/kotlin/metanit/chapter3_functionalProgramming/22_Lambda_Expressions.kt
@@ -0,0 +1,7 @@
+package edu.cesspool.metanit.chapter3_functionalProgramming
+
+// Л Я М Б Д А   В Ы Р А Ж Е Н И Я
+
+fun main(){
+    {println("Helloy, WWWWWWORLD!!!")}
+}
