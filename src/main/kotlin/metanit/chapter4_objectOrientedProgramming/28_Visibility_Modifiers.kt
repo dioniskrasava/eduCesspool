1c95547dfb508b668de30597a8c9609e4c13db27
dionis.krasava <dionis.krasava@gmail.com>
2025-12-25 19:02:08 +0700

    28+

--- src/main/kotlin/metanit/chapter4_objectOrientedProgramming/28_Visibility_Modifiers.kt
+++ src/main/kotlin/metanit/chapter4_objectOrientedProgramming/28_Visibility_Modifiers.kt
@@ -97,14 +97,109 @@ private fun ex2(){
 
 // Модификаторы конструкторов
 private fun ex3(){
-    open class Person constructor (val name: String){
+
+    fun example1(){
+        open class Person constructor (val name: String)
+
+        val tom = Person("Tom")
+        println(tom.name)
+    }
+
+    /** Пример, демонстрирующий приватный первичный конструктор, который
+     * невозможно вызвать вне класса. И поэтому мы создаем вторичный
+     * конструктор который как раз и может вызвать наш первичный.*/
+    fun example2(){
+        open class Person private constructor(val name: String){
+            var age: Int = 0
+
+            constructor(name: String, age: Int) : this(name){ // this - вызов приватного первичного конструктора
+                this.age = age
+            }
+
+        }
+
+        val tom = Person("Tom", 15)
+        println("Person ${tom.name}, age = ${tom.age}")
 
     }
 
-    val tom = Person("Tom")
-    println(tom.name)
+
+    /**Пример похож на предыдущий за исключением того что
+     * вторичный конструктор теперь имеет модификатор доступа - protected
+     * что теперь позволяет его вызвать ТОЛЬКО у класса наследника*/
+    fun example3(){
+        open class Person private constructor(val name: String){
+            var age: Int = 0
+
+            protected constructor(name: String, age: Int) : this(name){
+                this.age = age
+            }
+        }
+
+        class Employ(name: String, age: Int) : Person (name, age)
+
+        //val tom = Person("Tom", 16) // ERR
+        val tom = Employ("Tom", 16)
+
+        println(tom.name)
+        println(tom.age)
+
+    }
+
+
+    littleWrap("little example 1"){example1()}
+    littleWrap("little example 2"){example2()}
+    littleWrap("little example 3"){example3()}
+
+
+
 
 }
+
+// Модификаторы объектов и типов верхнего уровня
+/**
+ * В принципе тут просто описывается о том что
+ * - внутри публичных функций которые находятся в том же модуле
+ *      (как я понимаю это тот же пакет и файл одновременно)
+ *      что и
+ *      (приватные, интернальные, публичные) (классы, переменные, функции)
+ *      можно использовать эти объекты (классы, переменные, функции описанные выше)
+ *
+ *
+
+ * ```
+ * package base
+ *
+ * private val privateVal = 3
+ * internal val internalVal = 4
+ * val publicVal = 5
+ *
+ * private fun privateFun() = println("privateFn")
+ * fun internalFun() = println("internalFn")
+ * fun publicFun() = println("publicFn")
+ *
+ * private class PrivateClass(val name: String)
+ * internal class InternalClass(val name:String)
+ * class PublicClass(val name:String)
+ *
+ * fun printData(){
+ *     // внутри модуля доступны приватные идентификаторы
+ *     val privateClass= PrivateClass("Tom")
+ *     println(privateVal)
+ *     privateFun()
+ *
+ *     // внутри модуля доступны internal-идентификаторы
+ *     val internalClass= InternalClass("Tom")
+ *     println(internalVal)
+ *     internalFun()
+ *
+ *     // внутри модуля доступны public-идентификаторы
+ *     val publicClass= PublicClass("Tom")
+ *     println(publicVal)
+ *     publicFun()
+ * }
+ * ```
+ * */
 
 fun main(){
     wrap("EXAMPLE 1"){ex1()}
@@ -112,6 +207,13 @@ fun main(){
     wrap("EXMPLE 3 (MOD-CONST)"){ex3()}
 }
 
+
+//-------------НОВАЯ ВСПОМОГАТЕЛЬНАЯ ФУНКЦИЯ ДЛЯ ОТРИСОВКИ
+fun littleWrap(name: String, op: () -> Unit){
+    println("     ---------$name-----------")
+    op()
+    println("     -----------------------------------")
+}
 
 
 
