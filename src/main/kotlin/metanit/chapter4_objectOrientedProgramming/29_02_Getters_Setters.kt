e45f82333a82dc2da0411b5b3fffe4b3d14a962b
dionis.krasava <dionis.krasava@gmail.com>
2025-12-26 09:57:40 +0700

    30

--- src/main/kotlin/metanit/chapter4_objectOrientedProgramming/29_02_Getters_Setters.kt
+++ src/main/kotlin/metanit/chapter4_objectOrientedProgramming/29_02_Getters_Setters.kt
@@ -2,6 +2,32 @@ package edu.cesspool.metanit.chapter4_objectOrientedProgramming
 /**ИСПОЛЬЗОВАНИЕ ГЕТТЕРОВ И СЕТТЕРОВ В КЛАССАХ*/
 
 
+class Person(val firstname: String, val lastname: String){
+    var age = 0
+        // сеттер с валидацией
+        set(value) {
+            if ((value >= 0) and (value <= 120))
+                field = value
+        }
+
+    val fullname : String
+        // геттер вычисляемый
+        get() {
+            return "$firstname $lastname"
+        }
+}
+
+
 fun main(){
+    val bob = Person("Bob", "Smith")
+    println(bob.fullname)
+
+
+    bob.age = 32
+    println(bob.age)
+
+    bob.age = 578
+    println(bob.age)
 
 }
+
--- src/main/kotlin/metanit/chapter4_objectOrientedProgramming/30_Redefining_metods_and_Properties.kt
+++ src/main/kotlin/metanit/chapter4_objectOrientedProgramming/30_Redefining_metods_and_Properties.kt
@@ -6,7 +6,9 @@ import edu.cesspool.metanit.chapter2_basics.wrap
 // Переопределение методов и свойств
 
 fun main(){
-    wrap("EX 1"){ex1()}
+    wrap("EX 1 (overr field)  "){ex1()}
+    wrap("EX 2 (overr get-set)"){ex2()}
+    wrap("EX 3 (inheritors)"){ex3()}
 }
 
 // переопределение свойств
@@ -45,7 +47,73 @@ fun ex1(){
 
 // переопределение геттеров и сеттеров
 fun ex2(){
-    open class Person(){
+    open class Person(val name: String){
+        open val fullInfo: String
+            get() = "Person $name - age: $age"
 
+        open var age: Int = 0
+            set(value) {
+                if (value in 0..120) field = value
+            }
     }
+
+    /** чтобы переопределить сеттер - мы переопределяем все свойство*/
+    class Employee(name: String) : Person(name){
+        override val fullInfo: String
+            get() = "${super.fullInfo} теперь сотрудник!!!!"
+
+        override var age : Int = 18
+            set(value) {
+                if (value in 18..65) {
+                        field = value
+                        // выводит зеленым в консоль
+                        println("\u001B[32m   Возраст сотрудника изменен на $value\u001B[0m")
+                } else {
+                        // выводит красным в консоль
+                        println("\u001B[31m   ERR : Ты какой возраст решил поставить??? $value лет ?!?!?!? \u001B[0m") // красное ERR
+                }
+            }
+    }
+
+    val tom = Person("Tom")
+    println(tom.fullInfo)
+
+    val bob = Employee("Bob")
+    println(bob.fullInfo)
+
+    bob.age = 100
+    println(bob.fullInfo)
+
+    bob.age = 32
+    println(bob.fullInfo)
+
+}
+
+// переопределение в иерархии наследования классов
+fun ex3(){
+
+    /**ДОСТАТОЧНО ОДНОГО КЛЮЧЕВОГО СЛОВА OPEN
+     *  и это позволит всем наследникам переопределять метод супер-родителя*/
+
+    open class Person(val name: String){
+        open fun display() = println("Name : $name")
+    }
+
+    open class Employee(name: String, val company: String) : Person(name){
+        override fun display() = println("Name : $name. Company : $company")
+    }
+
+    class Manager(name: String, company: String) : Employee(name, company){
+        override fun display() =  println("Name: $name. Company: $company  Position: Manager")
+    }
+
+    val tom = Person("Tom")
+    val bob = Employee("Bob", "VTB")
+    val sem = Manager("Sem", "VTB")
+
+    tom.display()
+    bob.display()
+    sem.display()
+
+
 }
