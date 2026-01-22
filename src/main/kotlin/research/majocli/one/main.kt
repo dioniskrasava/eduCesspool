d67ea9bea3d67085fd85835ff95f6b60913d2f84
Ivan Belov <dionis.krasava@gmail.com>
2026-01-22 13:32:43 +0700

    majo cli+

--- src/main/kotlin/research/majocli/one/main.kt
+++ src/main/kotlin/research/majocli/one/main.kt
@@ -1,64 +1,22 @@
 package edu.cesspool.research.majocli.one
 
+import java.time.LocalDateTime
+
+/* Пишем простой пример CRUD приложения*/
+
+data class Note(
+    val id: Int,
+    val name: String,
+    val description: String = "",
+    val timeCreate : LocalDateTime = LocalDateTime.now()
+)
+
 
 fun main(){
-    wrapBasicCode { choosingAction()}
+    val notes = mutableListOf<Note>()
+
+    notes.add(Note(0, "Первая заметка"))
+    notes.add(Note(1, name = "Вторая заметка", "Описание второй заметки"))
+
+    println("Hello : $notes")
 }
-
-/**Обертка для текста программы. Выводит приветственное слово, создает рамки вверху и внизу*/
-fun wrapBasicCode(program : () -> Unit){
-    val infoBegin = """
-        |--------------------------------------------------------------------
-        |Привет! Вы используете тестовую версию majo-cli.
-        |Этот проект пишется в образовательных целях и не к чему не обязывает.
-        |   Возможно тут будет всё ужасно!
-        |   
-        |   
-    """.trimMargin()
-
-    val infoEnd = """
-        
-        
-        ---------------------------------------------------------------------
-    """.trimIndent()
-
-    println(infoBegin)
-    program()
-    println(infoEnd)
-
-}
-
-
-/**Выбор действия*/
-fun choosingAction(){
-    println(""" Выберите действие и нажмите ENTER:
-        | 1. Добавить активность
-        | 2. Вывести список активностей
-        | 3. Редактировать активность
-        | 4. Удалить активность
-        | 
-        | Ввод: 
-    """.trimMargin())
-
-    val a = readLine()
-
-    println("Вы выбрали $a")
-/*
-    when (a) {
-        1 -> addRecord(),
-        2 ->
-
-    }
-
-*/
-
-}
-
-fun addRecord(){
-
-}
-
-data class Record(
-    val id: Int,
-    val name: String
-)
