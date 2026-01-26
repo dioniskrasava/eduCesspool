8b0243d9e4f1cb3d292c181f1d1c4c7fec6227ad
Ivan Belov <dionis.krasava@gmail.com>
2026-01-26 13:37:21 +0700

    36_04 end

--- src/main/kotlin/metanit/chapter4_objectOrientedProgramming/36_04_By.kt
+++ src/main/kotlin/metanit/chapter4_objectOrientedProgramming/36_04_By.kt
@@ -1,15 +1,20 @@
 package edu.cesspool.metanit.chapter4_objectOrientedProgramming.less36_04
 
-// множественное делегирование
-// переопределение функций
+// Множественное делегирование
 
+// Переопределение функций
+
+// Делегирование свойств
 
 interface Messenger {
+    val versionMessenger: String
     fun sendTextMessage(message: String)
     fun sendVideoMessage()
 }
 
 class InstantMessenger(val programName: String) : Messenger{
+    override val versionMessenger: String = "v 0.0.1"
+
     override fun sendTextMessage(message: String) {
         println("($programName)Send message: $message")
     }
@@ -45,10 +50,13 @@ fun main(){
     val telegram = InstantMessenger("TG")
     val camera = PhotoCamera()
 
-    val honorx7c = SmartPhone("Honor x7c", telegram, camera)
+    val honorx7c = SmartPhone("Honor x7c", telegram, camera) // телефон
 
     honorx7c.sendTextMessage("Lol/Kek/Cheburek!")
     honorx7c.sendVideoMessage()
     honorx7c.takePhoto()
+
+    println("Version messenger - ${honorx7c.versionMessenger}") // Делегирование свойств - теперь у нашего телефона есть
+    // возможность использовать поле класса (свойство) как своё
 
 }
