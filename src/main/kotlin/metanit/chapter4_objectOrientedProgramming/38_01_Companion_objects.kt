28e356c00062c32a62e33f13dbad99b569f1d1a3
dionis.krasava <dionis.krasava@gmail.com>
2026-02-17 07:43:05 +0700

    38_01

--- src/main/kotlin/metanit/chapter4_objectOrientedProgramming/38_01_Companion_objects.kt
+++ src/main/kotlin/metanit/chapter4_objectOrientedProgramming/38_01_Companion_objects.kt
@@ -1,1 +1,32 @@
 package edu.cesspool.metanit.chapter4_objectOrientedProgramming.less38_01
+
+
+/**
+ * COMPANION OBJECT - объект который позволяет определить свойства и методы,
+ *                    которые будут общими для всех объектов этого класса.
+ *
+ *
+ *    т.е. companion object определяет свойства и методы КЛАССА В ЦЕЛОМ, а не объекта.
+ *
+ * */
+
+// САМЫЙ ПРОСТОЙ ПРИМЕР COMPANION OBJECT
+
+class Person (val name: String){
+
+    init {
+        println("Создано $counter объектов Person")
+        counter ++
+    }
+
+    companion object {
+        var counter = 0
+    }
+}
+
+
+fun main(){
+    Person("Вася")
+    Person("Петя")
+    Person("Маша")
+}
