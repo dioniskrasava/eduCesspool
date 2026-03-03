6d17f2d221acae9671ea8f3823c33400cbf5e31e
Ivan Belov <dionis.krasava@gmail.com>
2026-03-03 15:11:26 +0700

    40_05

--- src/main/kotlin/metanit/chapter5_generics/40_05_GenericConstraints.kt
+++ src/main/kotlin/metanit/chapter5_generics/40_05_GenericConstraints.kt
@@ -4,3 +4,36 @@ package edu.cesspool.metanit.chapter5_generics.less40_05
 //
 //    О Г Р А Н И Ч Е Н И Е   О Б О Б Щ Е Н И Й
 
+
+interface Message {
+    val text : String
+}
+
+interface Logger {
+    fun log()
+}
+
+class EmailMessage(override val text: String ): Message, Logger{
+    override fun log() {
+        println("Email: $text")
+    }
+}
+
+class SmsMessage(override val text: String) : Message, Logger{
+    override fun log() {
+        println("SMS: $text")
+    }
+}
+
+/** Параметр Т должен быть объектом класса наследующего интерфейс Message и Logger*/
+fun <T> send(message: T) where T: Message, T: Logger{
+    message.log()
+}
+
+fun main(){
+    val email = EmailMessage("Hello!")
+    val sms = SmsMessage("Goodbye")
+
+    send(email)
+    send(sms)
+}
