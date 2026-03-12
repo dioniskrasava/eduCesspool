de5791081086253ea565397b4e2793dbecfc08f1
Ivan Belov <dionis.krasava@gmail.com>
2026-03-12 11:11:23 +0700

    40_07+

--- src/main/kotlin/metanit/chapter5_generics/40_07_GenericConstraints.kt
+++ src/main/kotlin/metanit/chapter5_generics/40_07_GenericConstraints.kt
@@ -12,6 +12,38 @@ interface Logger{
     fun log()
 }
 
-class EmailMessage(
-    override val text
-) : Message
+class EmailMessage(override val text: String) : Message, Logger {
+    override fun log() {
+        println("Email: $text")
+    }
+}
+
+class SmsMessage(override val text: String) : Message, Logger {
+    override fun log() {
+        println("SMS: $text")
+    }
+}
+
+//-------------------------------------------------------------------------------
+//                                                                              -
+//   !  !  !  !     С  А  М  Ы  Й     В  А  Ж  Н  Ы  Й     М  О  М  Е  Н  Т     -
+//                                                                              -
+//          ОГРАНИЧЕНИЕ В КЛАССЕ СОЗДАЕТСЯ С ПОМОЩЬЮ КЛЮЧЕВОГО СЛОВА WHERE      -
+//                                                                              -
+//-------------------------------------------------------------------------------
+class Messenger<T>() where T: Message, T: Logger {
+    fun send(mes: T){
+        mes.log()
+    }
+}
+
+fun main(){
+    val email_message_1 = EmailMessage("Сообщение из электронной почты")
+    val sms_message_1 = SmsMessage("Привет, ты спишь?")
+
+    val outlook = Messenger<EmailMessage>()
+    val skype = Messenger<SmsMessage>()
+
+    outlook.send(email_message_1)
+    skype.send(sms_message_1)
+}
