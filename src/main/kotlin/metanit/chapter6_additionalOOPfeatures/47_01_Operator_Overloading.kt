d860abf29065c606639a4376a2b2a9d89731b33a
Ivan Belov <dionis.krasava@gmail.com>
2026-04-17 09:40:37 +0700

    47-01

--- src/main/kotlin/metanit/chapter6_additionalOOPfeatures/47_01_Operator_Overloading.kt
+++ src/main/kotlin/metanit/chapter6_additionalOOPfeatures/47_01_Operator_Overloading.kt
@@ -1,5 +1,17 @@
 package edu.cesspool.metanit.chapter6_additionalOOPfeatures
 
+/**
+ * В общем и целом можно переопределить почти все операторы которые уже существуют.
+ * Нельзя переопределить для обычных типов, для которых это уже определено (например для Int нельзя переписать сложение на свой лад)
+ * Можно определять оператор там где он не определен.
+ *
+ * Нельзя переопределить операторы :  ||  &&   ?:
+ * Можно переопределить операторы :
+ *
+ * Нельзя придумать свои операторы.
+ * У каждого оператора есть своё название на английском.
+ * */
+
 // Перегрузка операторов
 
 class Money(val amount: Int){
@@ -8,9 +20,28 @@ class Money(val amount: Int){
     }
 }
 
+// Можно переопределить оператора в виде функции расширения (уже после создания класса и для уже созданных классов)
+operator fun Money.minus(other: Money): Money{
+    return Money(this.amount - other.amount)
+}
+
+// Можно переопределить для стандартных типов и кастомных:
+// (теперь мы можем складывать обычные числа с деньгами)
+operator fun Int.plus(mon: Money): Money{
+    return Money(this + mon.amount)
+}
+
 fun main(){
     val m1 = Money(100)
     val m2 = Money(200)
     val m3: Money = m1 + m2
     println(m3.amount)
+
+
+    val m4: Money = m1 - m2
+    println(m4.amount)
+
+    val m5 = 5 + m1
+    println(m5.amount)
+
 }
