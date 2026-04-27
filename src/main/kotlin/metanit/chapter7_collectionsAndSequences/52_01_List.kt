a2e7a542f15f59a4dde060022c4f0e81f58707b6
Ivan Belov <dionis.krasava@gmail.com>
2026-04-27 12:40:47 +0700

    52_01

--- src/main/kotlin/metanit/chapter7_collectionsAndSequences/52_01_List.kt
+++ src/main/kotlin/metanit/chapter7_collectionsAndSequences/52_01_List.kt
@@ -5,10 +5,12 @@ package edu.cesspool.metanit.chapter7_collectionsAndSequences
 * */
 
 fun main(){
-    example()
+    //ex1()
+    //ex2()
+    ex3()
 }
 
-fun example(){
+fun ex1(){
     val numbers = listOf(1,2,3,4)
     val names = listOf("Tom", "Bob", "Sam")
 
@@ -21,4 +23,47 @@ fun example(){
 
     println("=====ВЫВОД ЧЕРЕЗ toString()=========")
     println(names.toString())
+}
+
+// получение элемента
+fun ex2(){
+    //get(index)
+    val people = listOf("Слава","Федя","Петя")
+    println(people.get(0))
+    println(people.get(1))
+    println(people.get(2))
+
+    println("---------")
+
+    // или через скобки []
+    println(people[0])
+    println(people[1])
+    println(people[2])
+
+    /**ЕСЛИ ВЫЗВАТЬ ПО НЕСУЩЕСТВУЮЩЕМУ ИНДЕКСУ, то выдаст ошибку
+     *
+     * можно использовать getOrNull() - выдаст элемент или null (если элемента не существует)
+     *
+     * есть еще getOrElse()
+     * */
+
+    // пример getOrElse()
+    val n = people.getOrElse(5){"Invalid index $it"}
+    println(n)  // Invalid index 5
+
+    val m = people.getOrElse(2){"Invalid index $it"}
+    println(m) // Петя
+
+}
+
+// получение части списка
+fun ex3(){
+    // метод subList
+    //       возвращает часть списка
+    //       в качестве параметров принимает начальный и конечный индексы
+
+    val people = listOf("Tom", "Sam", "Kate", "Bob", "Alice", "Mike")
+    val subPeople = people.subList(1, 4)
+    println(subPeople) // [Sam, Kate, Bob]
+
 }
---
+++ src/main/kotlin/metanit/chapter8_coroutines/69_01_Launch_Job.kt
@@ -0,0 +1,10 @@
+package edu.cesspool.metanit.chapter8_coroutines.less68_03
+
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+// launch - построитель корутин
+//
+// launch применяется когда не нужно воз
